aa61231e5e3981d5851a41db192ffa974642993f
Reemalshi <138518105+Reemalshi@users.noreply.github.com>
2023-07-31 19:08:04 +0400

    task 2 :  smallest missing positive integer.
    Given an unsorted integer array nums, find the smallest missing positive integer.

---
+++ taskt2.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class taskt2 {
+        public static void main(String[] args) {
+            Scanner read = new Scanner(System.in);
+            System.out.print("Enter the number of elements in the array: ");
+            int n = read.nextInt();
+
+            int[] nums = new int[n];
+            System.out.println("Enter the elements of the array:");
+
+            for (int i = 0; i < n; i++) {
+                nums[i] = read.nextInt();
+            }
+
+            int result = find(nums);
+            System.out.println("The smallest missing positive integer is: " + result);
+        }
+
+        public static int find(int[] nums) {
+            int n = nums.length;
+
+            for (int i = 0; i < n; i++) {
+                if (nums[i] <= 0) {
+                    nums[i] = n + 1;
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                int num = nums[i];
+                if (num <= n) {
+                    int x = num - 1;
+                    if (x >= 0 && x < n) {
+                        nums[x] = -nums[x];
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (nums[i] > 0) {
+                    return i + 1;
+                }
+            }
+
+            return n + 1;
+        }
+    }
