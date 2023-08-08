c112f3894ee666719f81cbc23581bb3f7cffd03e
Reemalshi <138518105+Reemalshi@users.noreply.github.com>
2023-08-08 16:23:20 +0400

    Using print statment

---
+++ Print/src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+//        USING PRINT WITH NUMBER:
+        System.out.println("reem");
+        System.out.println(12);
+        System.out.println(10.2);
+        System.out.println(true);
+
+        System.out.println(2 + 2);
+        System.out.println(2 - 1);
+        System.out.println(8 / 2);
+        System.out.println(3 * 3);
+
+        System.out.println(5 / 2);
+        System.out.println(0.5 / 2);
+        System.out.println(5 / 0.2);
+        System.out.println(0.5 / 0.2);
+
+        System.out.println(5 > 2);
+        System.out.println(5 < 2);
+        System.out.println(5 == 5);
+        System.out.println(5 != 6);
+
+//      USING PRINT WITH LATTER:
+        System.out.println(" j a v a ");
+        System.out.println(" jj aa vv aa ");
+        System.out.println(" jjj aaa vvv aaa ");
+        System.out.println(" jjjj aaaa vvvv aaaa ");
+
+        System.out.println("  _   _   _ ");
+        System.out.println(" |_| |_| |_| ");
+        System.out.println(" |_| |_| |_| ");
+        System.out.println(" |_| |_| |_| ");
+
+
+        System.out.println("  __   __   __ ");
+        System.out.println(" |  | |  | |  | ");
+        System.out.println("  __   __   __ ");
+        System.out.println(" |  | |  | |  | ");
+        System.out.println("  __   __   __ ");
+        System.out.println(" |  | |  | |  | ");
+        System.out.println("  __   __   __ ");
+
+//        USING PRINT WITH ALL:
+        System.out.println(4.0 * (1 - (1.0 / 3) + (1.0 / 5) - (1.0 / 7) + (1.0 / 9) - (1.0 / 11)));
+
+        System.out.println(" o o o m m m m m m m ");
+        System.out.println(" o o o m m m m m m m ");
+        System.out.println(" o o o a a a a a a a ");
+        System.out.println(" o o o a a a a a a a ");
+        System.out.println(" o o o n n n n n n n ");
+        System.out.println(" o o o n n n n n n n ");
+    }
+}
